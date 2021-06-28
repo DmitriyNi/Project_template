02b9a9d3580312cd9d013da737795f9208ce99b6
Dmitriy <streamlineddirudo@gmail.com>
2021-06-28 07:04:02 +0000

    наводим порядок

---
+++ docker/toolbox/date.py
@@ -0,0 +1,18 @@
+import datetime
+import random
+import re
+import bs4
+import requests
+# запись в index.html даты
+print(datetime.datetime.now())
+
+#user_query = 'цитата о'  #variable         #имеется запрос
+#blok_list = user_query.split()                        #разбиваем слова по пробелам
+#url_query = '%20'.join(blok_list)                     #разделяем их через %20
+#url = 'https://yandex.ru/search/?text=' + url_query + '&lr=213&p='+str(random.randint(1, 10))   #подставляем
+#r = requests.get(url)
+#with open('test.html', 'w') as output_file:
+#  findall(n, string, flags=0[optional]
+#quotes = soup.find_all('https', class_='text')
+ 
+
